07edfd0b887ce54b1a50d901de581442c7d08622
wulf <czh1983@gmail.com>
2024-08-01 22:27:41 -0300

    actualizacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
-import Dropdown from "./Dropdown"
+// import Dropdown from "./Dropdown"
 
 const Header = () => {
   return (
